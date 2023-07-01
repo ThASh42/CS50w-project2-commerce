b88e9746cd6446006cb9bc41850278b8f0a0313c
ThASh42 <th>
2023-07-01 14:32:19 +0300

    Add comments and remove import of timezone from Django utils

--- auctions/models.py
+++ auctions/models.py
@@ -1,27 +1,29 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
 
+
+# * Bid class
 class Bid(models.Model):
     bid = models.DecimalField(max_digits=5, decimal_places=2)
     time = models.DateTimeField()
 
 
+# * Model of update and creation time
 class Time(models.Model):
     update_time = models.DateField(auto_now=True)
     creation_time = models.DateField(auto_now=True)
     
-    
+    # provide "st", "nd", "rd" or "th"  suffix
     def provide_suffix(self, someday):
         return "th" if 11 <= someday <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(someday % 10, "th")
     
-    
+    # the function displays the creation time
     def print_creation_time(self):
         day = self.creation_time.day
         suffix = self.provide_suffix(day)
         return self.creation_time.strftime(f"%B %d{suffix}, %Y")
     
-    
+    # the function displays the update time
     def print_update_time(self):
         day = self.update_time.day
         suffix = self.provide_suffix(day)
@@ -32,6 +34,7 @@ class Time(models.Model):
         return f"Creation Time: {self.creation_time}, Update Time: {self.update_time}"
 
 
+# * Model of conditions
 class Condition(models.Model):
     CONDITION_CHOICES = [
         ('new', 'Brand New'),
@@ -46,6 +49,7 @@ class Condition(models.Model):
         return dict(Condition.CONDITION_CHOICES).get(self.name, self.name)
 
 
+# * Listing model
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
@@ -68,6 +72,7 @@ class Listing(models.Model):
         return self.title
 
 
+# * User model
 class User(AbstractUser):
     listings = models.ManyToManyField(Listing, blank=True, related_name="listings")
     pass
