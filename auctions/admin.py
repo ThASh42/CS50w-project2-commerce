5eaecac5e3ae8b34a52be24bbe6641ebaff50c04
ThASh42 <th>
2023-07-04 16:06:20 +0300

    Update Django admin interface

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 from .models import *
-# Register your models here.
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ["owner", "title", "condition", "category", "active_status"]
+    list_editable = ["condition", "category"]
+
+
 admin.site.register(User)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid)
 admin.site.register(Time)
