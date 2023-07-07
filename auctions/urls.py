dae8ab412a6c0d230a7fce7a6a37a3e8e5155630
ThASh42 <th>
2023-07-07 20:42:10 +0300

    Reorganize urlpatterns

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,21 +3,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Listing paths
     path("", views.index, name="index"),
-    path("watchlist", views.my_watchlist, name="my_watchlist"),
-    path("category", views.category, name="category"),
     path("listing/<int:listing_id>", views.listing, name="listing"),
-    path("<int:listing_id>/bid", views.bid, name="bid"),
-    path("create", views.create, name="create"),
-    path("<int:listing_id>/comment", views.comment, name="comment"),
-    
     path("<int:listing_id>/close_auction", views.close_auction, name="close_auction"),
+    path("<int:listing_id>/bid", views.bid, name="bid"),
+    path("<int:listing_id>/comment", views.comment, name="comment"),
     path("<int:listing_id>/status", views.status, name="status"),
-    path("my_listings", views.my_listings, name="my_listings"),
     
+    # Watchlist paths
+    path("watchlist", views.my_watchlist, name="my_watchlist"),
     path("<int:listing_id>/watchlist", views.watchlist, name="watchlist"),
     path("<int:listing_id>/delete-watchlist", views.delete_watchlist, name="delete_watchlist"),
     
+    # Other paths
+    path("create", views.create, name="create"),
+    path("category", views.category, name="category"),
+    path("my_listings", views.my_listings, name="my_listings"),
+    
+    # User paths
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
